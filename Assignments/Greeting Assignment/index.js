5d2fb20fc2733a1ed5f5c049a2ae15b6388aa970
Yaqub-Ibrahim <37085705+Yaqub-Ibrahim@users.noreply.github.com>
2018-11-02 13:04:54 -0600

    update

---
+++ Assignments/Greeting Assignment/index.js
@@ -0,0 +1,11 @@
+let hour = 12; //hours
+if (6<= hour)
+if(hour < 12)
+console.log("Good Morning!");
+if (12 <= hour)
+if (hour < 18)
+console.log("Good Afternoon!");
+if (18 <= hour)
+console.log("Good Evening!");
+if (hour > 24)
+console.log("There's only 24 hours in a day dummy!")
